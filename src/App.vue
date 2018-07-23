1d62e415fdbb6da67910930130428a290771334d
xg4 <xingor4@gmail.com>
2018-07-23 18:05:37 +0800

    feat: router view transition

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="app">
+  <div class="app">
     <header>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
       <router-link to="/about/news">About</router-link>
     </header>
     <section class="app-container">
-      <transition name="routerView">
+      <transition :name="transitionName">
         <keep-alive :include="keepAlive">
-          <router-view class="content" />
+          <router-view class="app-router-view" />
         </keep-alive>
       </transition>
     </section>
@@ -17,14 +17,28 @@
 </template>
 
 <script>
+const ROUTE_MAP = {}
+let prevIndex = 0
+
 export default {
   name: 'App',
   watch: {
     $route (to, from) {
       // router transition
-      const toDepth = to.path.split('/').length
-      const fromDepth = from.path.split('/').length
-      this.transitionName = toDepth < fromDepth ? 'xg-slideOutLeft' : 'xg-slideInRight'
+      if (prevIndex === window.history.length) {
+        // back
+        ROUTE_MAP[to.path] = false
+        this.transitionName = 'routerViewLeave'
+      } else {
+        // go
+        prevIndex = window.history.length
+        console.log(ROUTE_MAP[to.path])
+        this.transitionName = ROUTE_MAP[to.path] ? 'routerViewLeave' : 'routerViewEnter'
+        ROUTE_MAP[to.path] = true
+      }
+      // const toDepth = to.path.split('/').length
+      // const fromDepth = from.path.split('/').length
+      // this.transitionName = toDepth < fromDepth ? 'routerViewLeave' : 'routerViewEnter'
 
       // keep alive pages
       if (to.meta.keepAlive) {
@@ -36,16 +50,18 @@ export default {
   data () {
     return {
       keepAlive: [],
-      transitionName: 'xg-slideOutLeft'
+      transitionName: 'routerViewEnter'
     }
   }
 }
 </script>
 
 <style lang="scss">
-.app-container {
-  position: relative;
-  .content {
+.app {
+  &-container {
+    position: relative;
+  }
+  &-router-view {
     position: absolute;
     top: 0;
     left: 0;
